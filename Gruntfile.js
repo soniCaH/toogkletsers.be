0746acbc4281f5ac8ee5f6c72e48b09d8761a36c
Kevin Van Ransbeeck <Kevin.VanRansbeeck@amplexor.com>
2015-02-27 14:18:40 +0100

    imageoptim

--- Gruntfile.js
+++ Gruntfile.js
@@ -162,7 +162,7 @@ module.exports = function (grunt) {
   grunt.registerTask('build', [
     'svgmin',
     'grunticon',
-    // 'imagemin',
+    'imagemin',
     'compass:dev',
     'pleeease',
     'uglify'
